2b3178231aa04d81780aa3470024a509237e967e
mallikarjuna reddy akula <mallikarjunareddy.akula@s-force.org>
2019-07-24 12:35:20 +0530

    first commit

---
+++ mamaproject/src/main/java/com/example/demo/MamaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamaprojectApplication.class, args);
+	}
+
+}
